8d67bfe61c4d56857168a399eaa4f61d44403366
cakescripter <cakescripter@gmail.com>
2023-07-15 15:02:28 +0200

    Commit

--- testimonials-grid-section/tailwind.config.js
+++ testimonials-grid-section/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
+      colors: {
+        moderateViolet: 'hsl(263, 55%, 52%)',
+        veryDarkGrayishBlue: 'hsl(217, 19%, 35%)',
+        veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+        white: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(0, 0%, 81%)',
+        lightGrayishBlue: 'hsl(210, 46%, 95%)',
+      },
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
