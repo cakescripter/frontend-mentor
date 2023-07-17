3aafc1d4795a005f2f5d67fb1938212bae5369ad
cakescripter <cakescripter@gmail.com>
2023-07-17 19:15:09 +0200

    Commit

--- websites/url-shortening-api/tailwind.config.js
+++ websites/url-shortening-api/tailwind.config.js
@@ -5,9 +5,11 @@ module.exports = {
     extend: {
       colors: {
         cyan: 'hsl(180, 66%, 49%)',
+        lightCyan: 'hsl(180, 66%, 75%)',
         darkViolet: 'hsl(257, 27%, 26%)',
         red: 'hsl(0, 87%, 67%)',
         gray: 'hsl(0, 0%, 75%)',
+        darkGray: 'hsl(230, 25%, 95%)',
         grayishViolet: 'hsl(257, 7%, 63%)',
         veryDarkBlue: 'hsl(255, 11%, 22%)',
         veryDarkViolet: 'hsl(260, 8%, 14%)',
