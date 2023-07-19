a29f06cb7aa05287cb5bf113708c9e5885d033f4
cakescripter <cakescripter@gmail.com>
2023-07-19 12:10:20 +0200

    Commit

--- websites/url-shortening-api/script.js
+++ websites/url-shortening-api/script.js
@@ -96,8 +96,15 @@ async function fetchshrco(url) {
   let request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
   let response = await request.json();
   let shortened = response.result.full_short_link;
-  return shortened;
+
+  function removeHttps(url) {
+    return url.replace(/^https?:\/\//, '');
+  }
+  return removeHttps(shortened);
 }
+
+
+
 
 form.addEventListener("submit", handleFormSubmission);
 btn.addEventListener('click', navToggle);
