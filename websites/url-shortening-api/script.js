04bdf7d556022f1967393f6ccb23ff70f109e995
cakescripter <cakescripter@gmail.com>
2023-07-19 03:00:29 +0200

    handles API

--- websites/url-shortening-api/script.js
+++ websites/url-shortening-api/script.js
@@ -19,7 +19,7 @@ function isOddEven() {
   return isOdd ? "odd" : "even";
 }
 
-function createLinkElement(link) {
+function createLinkElement(link, shortenedLink) {
   const newLinkElement = document.createElement("div");
   newLinkElement.classList.add("link-container");
 
@@ -29,7 +29,7 @@ function createLinkElement(link) {
   newLinkElement.innerHTML = `
     <p>${link}</p>
     <div class="link">
-      <div class="font-bold text-cyan">${link}</div>
+      <div class="font-bold text-cyan">${shortenedLink}</div>
       <button class="copy ${x}">Copy</button>
     </div>
   `;
@@ -45,17 +45,34 @@ function clearInputField(input) {
   input.value = "";
 }
 
-function handleFormSubmission(event) {
+async function handleFormSubmission(event) {
   event.preventDefault(); // Prevent form submission
 
   const input = document.querySelector("#link-input");
   const link = input.value.trim();
 
-  if (isURL(link)) {
-    const newLinkElement = createLinkElement(link);
+  if (!isURL(link)) {
+    document.querySelector("#err-msg").textContent = "Please enter a valid URL.";
+    return;
+  }
+
+  try {
+    const shortenedLink = await fetchshrco(link);
+    const newLinkElement = createLinkElement(link, shortenedLink);
     addLinkToLinkList(newLinkElement);
     clearInputField(input);
+  } catch (error) {
+    document.querySelector("#err-msg").textContent =
+      "Something went wrong with fetching the shortened link. Please try again later.";
+    console.error("Error fetching the shortened link:", error);
   }
+}
+
+async function fetchshrco(url) {
+  let request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
+  let response = await request.json();
+  let shortened = response.result.full_short_link;
+  return shortened;
 }
 
 // Event Listener
